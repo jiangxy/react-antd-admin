a87846c3aec00b2df875ce64dde1934f48c3621e
jiangxiyang <jiangxiyang@weidian.com>
2016-07-11 16:52:56 +0800

    update config

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ module.exports = {
   name: 'XX管理后台',  // 项目的名字
   footer: 'xxx版权所有 © 2015-2099',  // footer中显示的字
 
-  apiHost: 'http://localhost:8088',  // 调用ajax接口的地址, 默认值空, 如果是跨域的, 服务端要支持CORS
+  apiHost: '',  // 调用ajax接口的地址, 默认值空, 如果是跨域的, 服务端要支持CORS
   apiPath: '/api',  // ajax请求的路径
 
   loginValidate: '/login/validate',  // 用于校验用户是否登录的接口
