b9fc1de2e2c180ea439490cf1b4b31058af2d3ea
jiangxiyang <jiangxiyang@weidian.com>
2016-08-04 11:08:06 +0800

    add react-hot-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
-var webpack = require('webpack');
+const webpack = require('webpack');
+
+// 将babel-loader的配置独立出来, 因为webpack的限制: http://stackoverflow.com/questions/33117136/how-to-add-a-query-to-a-webpack-loader-with-multiple-loaders
+const babelLoaderConfig = {
+  presets: ['es2015', 'stage-0', 'react'],  // 开启ES6、部分ES7、react特性, preset相当于预置的插件集合
+  plugins: [['antd', {'style': true}]],  // antd模块化加载, https://github.com/ant-design/babel-plugin-antd
+};
 
 module.exports = {
   entry: [
-    'webpack/hot/dev-server',  // 调试时需要
+    'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
     'babel-polyfill',  // 可以使用完整的ES6特性, 大概增加100KB
     './src/index.js',  // 编译的入口
   ],
@@ -10,7 +17,6 @@ module.exports = {
   output: {  // 输出的目录和文件名
     path: __dirname + '/dist',
     filename: 'bundle.js',
-    publicPath: 'http://mycdn.com/', // require图片时返回地址的前缀
   },
 
   resolve: {
@@ -25,11 +31,7 @@ module.exports = {
     loaders: [  // 定义各种loader
       {
         test: /\.jsx?$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'stage-0', 'react'],  // 开启ES6、部分ES7、react特性, preset相当于预置的插件集合
-          plugins: [['antd', {'style': true}]],  // antd模块化加载, https://github.com/ant-design/babel-plugin-antd
-        },
+        loaders: ['react-hot', 'babel-loader?' + JSON.stringify(babelLoaderConfig)],  // react-hot-loader可以不用刷新页面, 如果用普通的dev-server的话会自动刷新页面
         exclude: /node_modules/,
       }, {
         test: /\.css$/,
