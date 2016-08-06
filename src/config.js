280c4fdd8bed3e95268853d717959c4ca8122268
jiangxiyang <jiangxiyang@weidian.com>
2016-08-06 20:22:48 +0800

    update global config

--- src/config.js
+++ src/config.js
@@ -1,13 +1,18 @@
-/*
+/**
  * 定义整个项目的全局配置
  */
 
 module.exports = {
-  name: 'XX管理后台',  // 项目的名字
+  name: 'ABC管理后台',  // 项目的名字
   footer: 'xxx版权所有 © 2015-2099',  // footer中显示的字
 
-  apiHost: '',  // 调用ajax接口的地址, 默认值空, 如果是跨域的, 服务端要支持CORS
-  apiPath: '/api',  // ajax请求的路径
+  debug: true,  // 是否开启debug模式
 
-  loginValidate: '/login/validate',  // 用于校验用户是否登录的接口
+  api: {  // 对后端请求的相关配置
+    host: '',  // 调用ajax接口的地址, 默认值空, 如果是跨域的, 服务端要支持CORS
+    path: '/api',  // ajax请求的路径
+
+    login: '/login',  // 用于校验用户是否登录的接口, 要返回当前登录的用户名
+  },
+
 }
