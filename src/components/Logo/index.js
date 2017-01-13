81d0fa74066844d99d3bb52c27b5df893bb4aca0
jiangxiyang <jiangxiyang@weidian.com>
2017-01-14 00:44:07 +0800

    logo折叠

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -5,13 +5,14 @@ import './index.less';
 /**
  * 定义Logo组件
  */
-class Logo extends React.Component {
+class Logo extends React.PureComponent {
 
   render() {
     return (
-      <div className="ant-layout-logo">
-        <div className="logo-text">
-          <a href="#">{globalConfig.name}</a>
+      <div className={this.props.collapse ? "ant-layout-logo-collapse" : "ant-layout-logo-normal"}>
+        <div className="ant-layout-logo-text">
+          {/*侧边栏折叠的时候只显示一个字*/}
+          <a href="#">{this.props.collapse ? globalConfig.name[0] : globalConfig.name}</a>
         </div>
       </div>
     );
