2801cf07144a1969a7a0377ef259edd727bfc899
jiangxiyang <jiangxiyang@weidian.com>
2017-01-14 18:46:29 +0800

    update Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,132 @@
 import React from 'react';
-import {Icon, Menu, Dropdown} from 'antd';
+import {Link} from 'react-router';
+import {Icon, Menu} from 'antd';
+import Logger from '../../utils/Logger';
 import globalConfig from 'config';
 import './index.less';
+import {headerMenu} from 'menu';
 
 const SubMenu = Menu.SubMenu;  // 为了使用方便
+const MenuItem = Menu.Item;
+const MenuItemGroup = Menu.ItemGroup;
+
+const logger = Logger.getLogger('Header');
 
 /**
  * 定义Header组件, 包括登录/注销的链接, 以及一些自定义链接
  */
-class Header extends React.Component {
+class Header extends React.PureComponent {
+
+  // parse菜单的过程和sidebar组件差不多, copy&paste
+
+  transFormMenuItem(obj, paths) {
+    const parentPath = paths.join('/');
+    logger.debug('transform %o to path %s', obj, parentPath);
+
+    return (
+      <MenuItem key={obj.key}>
+        {obj.icon && <Icon type={obj.icon}/>}
+        {obj.url ? <a target="_blank" href={obj.url}>{obj.name}</a> : <Link to={`/${parentPath}`}>{obj.name}</Link>}
+      </MenuItem>
+    );
+  }
+
+  componentWillMount() {
+    const paths = [];
+
+    // 这一项菜单是必须有的, 不需要在配置文件里配置
+    this.logoutMenuItem = <MenuItem key="logout">
+      <Icon type="logout"/>
+      <a href={`${globalConfig.getAPIPath()}${globalConfig.login.logout}`}>注销</a>
+    </MenuItem>;
+
+    // header右侧必须是用户菜单
+    let userMenuItems = null;
+
+    const menu = headerMenu.map((level1) => {
+      paths.push(level1.key);
+      let transformedLevel1Menu;
+
+      if (level1.child) {
+        const level2menu = level1.child.map((level2) => {
+          paths.push(level2.key);
+
+          if (level2.child) {
+            const level3menu = level2.child.map((level3) => {
+              paths.push(level3.key);
+              const tmp = this.transFormMenuItem(level3, paths);
+              paths.pop();
+              return tmp;
+            });
+
+            paths.pop();
+
+            // 与sidebarMenu不同的是这里返回MenuItemGroup
+            return (
+              <MenuItemGroup key={level2.key}
+                             title={level2.icon ? <span><Icon type={level2.icon} />{` ${level2.name}`}</span> : <span>{level2.name}</span>}>
+                <Menu.Divider />
+                {level3menu}
+              </MenuItemGroup>
+            );
+          } else {
+            const tmp = this.transFormMenuItem(level2, paths);
+            paths.pop();
+            return tmp;
+          }
+        });
+
+        paths.pop();
+
+        transformedLevel1Menu = (
+          <SubMenu key={level1.key}
+                   title={level1.icon ? <span><Icon type={level1.icon} />{level1.name}</span> : level1.name}>
+            {level2menu}
+          </SubMenu>
+        );
+      } else {
+        transformedLevel1Menu = this.transFormMenuItem(level1, paths);
+        paths.pop();
+      }
+
+      // 顶层菜单parse完毕后, 先不要直接返回, 如果用户在config中定义了用户菜单, 要单独处理
+      if (level1.key === 'userMenu') {
+        userMenuItems = transformedLevel1Menu.props.children;  // 注意这个直接读取props的逻辑
+        return null;
+      } else {
+        return transformedLevel1Menu;
+      }
+    });
+
+    this.menu = menu;
+
+    // 如果用户没有定义userMenu怎么处理? 或者定义了key=userMenu, 但是没有定义child怎么办?
+    // 所以这里判断下, 如果用户没有定义, 就把userMenu作为普通的MenuItem, 而不是SubMenu
+
+    const userMenu = userMenuItems && userMenuItems[0] ?  // 这个判断方式有一点hack
+      <SubMenu title={<span><Icon type="user" />{this.props.userName}</span>}>
+        {userMenuItems}
+      </SubMenu>  // 有子菜单就作为SubMenu
+      :
+      <MenuItem key="userMenu">
+        <Icon type="user"/>
+        <a href="#">{this.props.userName}</a>
+      </MenuItem>;  // 没有子菜单就作为MenuItem
+
+    this.userMenu = userMenu;
+  }
+
+  // FIXME: 这里其实有个bug, 如果菜单名称很长可能会导致overflow, 出现滚动条
+  // 暂时无法解决..., 怎么调css都不对
 
   render() {
     return (
-      <div className='ant-layout-header'>
-        {/*定义header中的菜单*/}
+      <div className="ant-layout-header">
+        {/*定义header中的菜单, 从右向左依次是注销/用户菜单/其他自定义菜单*/}
         <Menu className="header-menu" mode="horizontal">
-          <SubMenu title={<span><Icon type="user" />{this.props.userName}</span>}>
-            <Menu.Item key="logout">
-              <a href={`${globalConfig.getAPIPath()}${globalConfig.login.logout}`}>注销</a>
-            </Menu.Item>
-          </SubMenu>
+          {this.logoutMenuItem}
+          {this.userMenu}
+          {this.menu}
         </Menu>
       </div>
     );
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,5 @@
 /**
- * 定义sidebar中的菜单项
+ * 定义sidebar和header中的菜单项
  *
  * 一些约定:
  * 1.菜单最多3层;
@@ -10,7 +10,8 @@
 // 其实理论上可以嵌套更多层菜单的, 但是我觉得超过3层就不好看了
 // 可用的图标见这里: https://ant.design/components/icon-cn/
 
-module.exports = [
+// 定义siderbar菜单
+const sidebarMenu = [
   {
     key: 'index',  // route时url中的值
     name: '菜单哈哈哈',  // 在菜单中显示的名称
@@ -123,6 +124,125 @@ module.exports = [
           {
             key: '666bb',
             name: '选项6',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default sidebarMenu;
+
+// 定义header菜单, 格式和sidebar是一样的
+// 特殊的地方在于, 我规定header的最右侧必须是用户相关操作的菜单, 所以定义了一个特殊的key
+// 另外注意这个菜单定义的顺序是从右向左的, 因为样式是float:right
+export const headerMenu = [
+  {
+    // 一个特殊的key, 定义用户菜单, 在这个submenu下面设置icon/name不会生效
+    key: 'userMenu',
+    child: [
+      {
+        key: 'modifyUser',
+        name: '修改用户信息',
+        icon: 'bulb',
+        // 对于headerMenu的菜单项, 可以让它跳到外部地址, 如果设置了url属性, 就会打开一个新窗口
+        // 如果不设置url属性, 行为和sidebarMenu是一样的, 激活特定的组件, 注意在index.js中配置好路由, 否则会404
+        url: 'http://jxy.me',
+      },
+      {
+        key: 'user222',
+        name: '药药切克闹',
+        icon: 'rocket',
+      },
+      {
+        key: 'user333',
+        name: '选项3',
+        child: [
+          {
+            key: 'user333aaa',
+            name: 'user333aaa',
+            icon: 'windows',
+          },
+          {
+            key: 'user333bbb',
+            name: 'user333bbb',
+            url: 'http://jxy.me',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    key: 'headerMenu2',
+    name: 'header菜单',
+    icon: 'team',
+    child: [
+      {
+        key: 'headerMenu111',
+        name: '菜单项1',
+        icon: 'windows',
+        url: 'http://jxy.me',
+      },
+      {
+        key: '菜单项2',
+        name: '短信表管理',
+        url: 'http://jxy.me',
+      },
+      {
+        key: '菜单项3',
+        name: '选项3',
+        icon: 'chrome',
+        url: 'http://jxy.me',
+      },
+    ],
+  },
+  {
+    key: 'headerMenu3',
+    name: '我没有子菜单',
+    icon: 'setting',
+    url: 'http://jxy.me',
+  },
+  {
+    key: 'headerMenu4',
+    name: '我也没有子菜单',
+    icon: 'shopping-cart',
+  },
+  {
+    key: 'headerMenu5',
+    name: '我没有图标',
+    child: [
+      {
+        key: 'headerMenu5000000',
+        name: '二级导航无子菜单',
+      },
+      {
+        key: 'headerMenu51111',
+        name: '三级导航',
+        icon: 'laptop',
+        child: [
+          {
+            key: 'headerMenu51111aa',
+            name: '选项6',
+            icon: 'meh',
+          },
+          {
+            key: 'headerMenu51111bb',
+            name: '选项7',
+            icon: 'inbox',
+          },
+        ],
+      },
+      {
+        key: 'headerMenu52222',
+        name: '三级导航无图标',
+        child: [
+          {
+            key: 'headerMenu52222aa',
+            name: '选项8',
+          },
+          {
+            key: 'headerMenu52222bb',
+            name: '选项9',
           },
         ],
       },
